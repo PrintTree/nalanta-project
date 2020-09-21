61e97397bc64a2dad884ef7dbbafead40588b415
Nalanta <565357130@qq.com>
2020-09-22 00:40:25 +0800

    add gitignore; add json api;

--- nalanta-core/src/main/java/org/nalanta/core/json/JsonArray.java
+++ nalanta-core/src/main/java/org/nalanta/core/json/JsonArray.java
@@ -1,4 +1,19 @@
 package org.nalanta.core.json;
 
+import java.util.List;
+
 public class JsonArray {
+
+    JsonArray from(String jsonString) {
+        return this;
+    }
+
+    JsonArray from(List<Object> jsonList) {
+        return this;
+    }
+
+    String stringify() {
+        return null;
+    }
+
 }
--- nalanta-core/src/main/java/org/nalanta/core/json/JsonObject.java
+++ nalanta-core/src/main/java/org/nalanta/core/json/JsonObject.java
@@ -1,12 +1,58 @@
 package org.nalanta.core.json;
 
+import java.util.Map;
+
 public class JsonObject {
 
     JsonObject from(String jsonString) {
         return this;
     }
 
-    String string() {
+    JsonObject from(Map<String, Object> jsonMap) {
+        return this;
+    }
+
+    String stringify() {
         return null;
+    }
+
+    String string(String key) {
+        return null;
+    }
+
+    JsonObject string(String key, String value) {
+        return this;
+    }
+
+    Integer integer(String key) {
+        return null;
+    }
+
+    JsonObject integer(String key, Integer value) {
+        return this;
+    }
+
+    Double number(String key) {
+        return null;
+    }
+
+    JsonObject number(String key, Double value) {
+        return this;
+    }
+
+    JsonObject object(String key) {
+        return null;
+    }
+
+    JsonObject object(String key, JsonObject value) {
+        return this;
+    }
+
+    JsonArray array(String key) {
+        return null;
+    }
+
+    JsonObject array(String key, JsonObject value) {
+        return this;
     }
 }
